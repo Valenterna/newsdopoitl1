b0c5b83bf57b1a87b7eefafb7883b02414427fd1
baboshko12@gmail.com <HP@DESKTOP-DN1L7RB>
2024-01-09 21:00:38 +0300

    Модульное тестирование

---
+++ process.cpp
@@ -0,0 +1,7 @@
+#include "processing.h"
+
+int process(bank* array[], int size)
+{
+    
+    return 0;
+}
---
+++ processing.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef PROCESSING_H
+#define PROCESSING_H
+
+#include "bank.h"
+
+int process(bank* array[], int size);
+
+#endif
